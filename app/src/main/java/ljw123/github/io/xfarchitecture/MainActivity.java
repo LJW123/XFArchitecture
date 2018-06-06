3f5bfed3903bd42ea278f4b726529a0fc8e3f187
LXR <18203660536@163.com>
2018-06-06 15:47:24 +0800

    架构项目初始化

---
+++ app/src/main/java/ljw123/github/io/xfarchitecture/MainActivity.java
@@ -0,0 +1,13 @@
+package ljw123.github.io.xfarchitecture;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
